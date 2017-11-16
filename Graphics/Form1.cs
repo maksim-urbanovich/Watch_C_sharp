47aa6418221ec2cafe6b70ccbfc3364208bae828
maksim-urbanovich <MaximusUrb@gmail.com>
2017-11-16 19:06:09 +0300

    Modified Digits

--- Graphics/Form1.cs
+++ Graphics/Form1.cs
@@ -27,7 +27,7 @@ namespace Graphics
         System.Drawing.Font drawFont = new System.Drawing.Font("Arial", 16);
         System.Drawing.SolidBrush drawBrush = new System.Drawing.SolidBrush(System.Drawing.Color.DarkRed);
 
-        
+
 
 
 
@@ -45,35 +45,42 @@ namespace Graphics
             // Hook up timer's tick event handler.
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
         }
-        
+
         private void Clock_Draw(System.Drawing.Graphics g)
         {
             r_clock = r + 15;
             g.DrawEllipse(System.Drawing.Pens.Black, x1 - r_clock, y1 - r_clock, 2 * r_clock, 2 * r_clock);
             g.FillEllipse(brush, x1 - r_c, y1 - r_c, 2 * r_c, 2 * r_c);
 
-            for (int i = 1; i <= 60; i++) {
+            for (int i = 1; i <= 60; i++)
+            {
                 int x0, y0, x3, y3, x_dig, y_dig;
                 double a0 = -Math.PI / 2 + (i * Math.PI / 30);
                 x3 = x1 + (int)(r_clock * Math.Cos(a0));
                 y3 = y1 + (int)(r_clock * Math.Sin(a0));
                 if (i % 5 == 0)
                 {
+                    int i1 = Math.Cos(a0) > 0 ? 1 : (int)(Math.Cos(a0));
+                    int i2 = Math.Sin(a0) > 0 ? 1 : (int)(Math.Sin(a0));
                     x0 = x1 + (int)((r_clock - 10) * Math.Cos(a0));
                     y0 = y1 + (int)((r_clock - 10) * Math.Sin(a0));
-                    x_dig = x1 + (int)(r_clock * Math.Cos(a0));
-                    y_dig = y1 - 20 + (int)(r_clock * Math.Sin(a0));
+
+                    x_dig = x1 + (int)((r_clock - 10) * Math.Cos(a0)) - 12 * i1 + 12 * i2;
+                    y_dig = y1 + (int)((r_clock - 10)* Math.Sin(a0)) - + 8 * i1 - 4 * i2;
+                    Point p_dig = new Point(x_dig, y_dig);
 
                     g.DrawLine(pen_hour, x0, y0, x3, y3);
-                    g.DrawString("" + (i / 5), drawFont, drawBrush, x_dig, y_dig);
+                    g.DrawString("" + (i / 5), drawFont, drawBrush, p_dig);
+                    //g.DrawString("" + x_dig, drawFont, drawBrush, 150, 150 + 4 * i);
+                   // g.DrawString("" + y_dig, drawFont, drawBrush, 200, 150+4*i);
+                    //g.DrawString("" + (int)(a0*100), drawFont, drawBrush, 250, 150 + 4 * i);
                 }
-                else 
+                else
                 {
                     x0 = x1 + (int)((r_clock - 5) * Math.Cos(a0));
                     y0 = y1 + (int)((r_clock - 5) * Math.Sin(a0));
                     g.DrawLine(pen_1min, x0, y0, x3, y3);
-                }               
- 
+                }
             }
 
         }
@@ -88,7 +95,7 @@ namespace Graphics
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            System.Drawing.Graphics g = e.Graphics;           
+            System.Drawing.Graphics g = e.Graphics;
             ClockHands_Draw(g);
             Clock_Draw(g);
 
@@ -103,15 +110,15 @@ namespace Graphics
             x1_hour = x1; y1_hour = y1;
 
             //задаем радиусы
-            r = 150; 
+            r = 150;
             r2 = r;
             r3 = 100;
 
             //задаем начальное значение угла поворота в зависимости от текущего времени
-            a = Math.PI / 2 - (now_time.Second * Math.PI / 30);    
+            a = Math.PI / 2 - (now_time.Second * Math.PI / 30);
             a_min = Math.PI / 2 - (now_time.Minute * Math.PI / 30) - (now_time.Second * Math.PI / (60 * 30));
             a_hour = Math.PI / 2 - (now_time.Hour * Math.PI / 6) - (now_time.Minute * Math.PI / (60 * 6));
-            
+
             //определяем конец секундной стрелки с учетом центра экрана
             x2 = x1 + (int)(r * Math.Cos(a));
             y2 = y1 - (int)(r * Math.Sin(a));
@@ -123,8 +130,6 @@ namespace Graphics
             //определяем конец часовой стрелки с учетом центра экрана
             x2_hour = x1_hour + (int)(r3 * Math.Cos(a_hour));
             y2_hour = y1_hour - (int)(r3 * Math.Sin(a_hour));
-
-
         }
 
         private string toTimeString(int num)
@@ -160,79 +165,5 @@ namespace Graphics
             y2_hour = y1_hour - (int)(r3 * Math.Sin(a_hour));
             Invalidate(); //вынудительный вызов перерисовки (Paint)
         }
-
-        /*private void label1_Click(object sender, EventArgs e)
-        {
-            DateTime now_time1 = DateTime.Now;
-            label1.Text = now_time1.Hour.ToString() + ":" + now_time1.Minute.ToString() + ":" + now_time1.Minute.ToString();
-            now_time1.Ticks
-        }*/
     }
 }
-
-/*
-    private void InitializeTimer()
-        {
-// Run this procedure in an appropriate event.
-          counter = 0;
-          timer1.Interval = 1600;
-          timer1.Enabled = true;
-// Hook up timer's tick event handler.
-          this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-         }
-    private void timer1_Tick(object sender, System.EventArgs e)
-    {
-        if (counter >= 10)
-        {
-            // Exit loop code.
-            timer1.Enabled = false;
-            counter = 0;
-        }
-        else
-        {
-            // Run your procedure here.
-            // Increment counter.
-            counter = counter + 1;
-            label1.Text = "Procedures Run: " + counter.ToString();
-        }
-    }
-*/
-
-/* private void button1_Click(object sender, EventArgs e)
- {
-     System.Drawing.Pen myPen;
-
-     myPen = new System.Drawing.Pen(System.Drawing.Color.Red);
-     System.Drawing.Graphics formGraphics = this.CreateGraphics();
-     formGraphics.DrawLine(myPen, 0, 0, 200, 200);
-     myPen.Dispose();
-     formGraphics.Dispose();
- }
-
- private void button2_Click(object sender, EventArgs e)
- {
-     System.Drawing.Graphics formGraphics = this.CreateGraphics();
-     string drawString = "Визуальное программирование";
-     System.Drawing.Font drawFont = new System.Drawing.Font("Arial", 16);
-     System.Drawing.SolidBrush drawBrush = new
-     System.Drawing.SolidBrush(System.Drawing.Color.DarkRed);
-     float x = 50.0f;
-     float y = 50.0f;
-     formGraphics.DrawString(drawString, drawFont, drawBrush, x, y);
-     drawFont.Dispose();
-     drawBrush.Dispose();
-     formGraphics.Dispose();   
-
- }
-
- private void button3_Click(object sender, EventArgs e)
- {
-     //Эллипс и прямоугольник
-     System.Drawing.Graphics graphics = this.CreateGraphics();
-     System.Drawing.Rectangle rectangle = new System.Drawing.Rectangle(
-         10, 10, 110, 150);
-     graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
-     graphics.DrawRectangle(System.Drawing.Pens.Red, rectangle);            
-     graphics.Dispose();
-
- }*/
